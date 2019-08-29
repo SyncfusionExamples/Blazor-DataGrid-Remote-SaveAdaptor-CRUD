facdb0686be1a1f55649eb94a70c2677283db68c
Rahul-Narayanasamy <rahulkishore227@gmail.com>
2019-08-29 18:29:56 +0530

    sample added

---
+++ README.md
@@ -0,0 +1,10 @@
+# EJ2-DataGrid-Blazor-RemoteSaveAdaptor-CRUD
+This example shows that how to bind local data and perform CRUD at server by using RemoteSaveAdaptor.
+
+**Syncfusion EJ2 Grid RemoteSaveAdaptor Sample.**
+
+This example shows that how to bind local data and perform CRUD at server by using RemoteSaveAdaptor.
+
+You may need to perform all Grid Actions(like paging, filtering, sorting) in client-side except the CRUD operations, that should be interacted with server-side to persist data. It can be achieved in Grid by using **RemoteSaveAdaptor**.
+
+Datasource must be set to **json** property and set **RemoteSaveAdaptor** to the **adaptor** property. CRUD operations can be mapped to server-side using **insertUrl**, **updateUrl**, **removeUrl** properties.
---
+++ RemoteSaveAdaptor/RemoteSaveAdaptor/RemoteSaveAdaptor.Server/Controllers/DefaultController.cs
@@ -0,0 +1,93 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
+using RemoteSaveAdaptor.Shared;
+
+
+namespace RemoteSaveAdaptor.Server.Controllers
+{
+
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpPost]
+        [Route("api/Default/Update")]
+        public object Update([FromBody]CRUDModel<OrdersDetails> value)
+        {
+            var ord = value.Value;
+            OrdersDetails val = OrdersDetails.GetAllRecords().Where(or => or.OrderID == ord.OrderID).FirstOrDefault();
+            val.OrderID = ord.OrderID;
+            val.EmployeeID = ord.EmployeeID;
+            val.CustomerID = ord.CustomerID;
+            val.Freight = ord.Freight;
+            val.ShipCity = ord.ShipCity;
+            return value.Value;
+        }
+        [HttpPost]
+        [Route("api/Default/Insert")]
+        public object Insert([FromBody]CRUDModel<OrdersDetails> value)
+        {
+            OrdersDetails.GetAllRecords().Insert(0, value.Value);
+            return value.Value;
+        }
+        [HttpPost]
+        [Route("api/Default/Delete")]
+        public object Delete([FromBody]CRUDModel<OrdersDetails> value)
+        {
+            OrdersDetails.GetAllRecords().Remove(OrdersDetails.GetAllRecords().Where(or => or.OrderID == int.Parse(value.Key.ToString())).FirstOrDefault());
+            return value;
+        }
+        public class CRUDModel<T> where T : class
+        {
+
+            [JsonProperty("action")]
+            public string Action { get; set; }
+            [JsonProperty("table")]
+            public string Table { get; set; }
+            [JsonProperty("keyColumn")]
+            public string KeyColumn { get; set; }
+            [JsonProperty("key")]
+            public object Key { get; set; }
+            [JsonProperty("value")]
+            public T Value { get; set; }
+            [JsonProperty("added")]
+            public List<T> Added { get; set; }
+            [JsonProperty("changed")]
+            public List<T> Changed { get; set; }
+            [JsonProperty("deleted")]
+            public List<T> Deleted { get; set; }
+            [JsonProperty("params")]
+            public IDictionary<string, object> Params { get; set; }
+        }
+        [Serializable]
+        public class Orders
+        {
+            public Orders()
+            {
+
+            }
+            public Orders(long OrderId, string CustomerId, int EmployeeId, double Freight, DateTime OrderDate, string ShipCity)
+            {
+                this.OrderID = OrderId;
+                this.CustomerID = CustomerId;
+                this.EmployeeID = EmployeeId;
+                this.Freight = Freight;
+                this.OrderDate = OrderDate;
+                this.ShipCity = ShipCity;
+            }
+            public long OrderID { get; set; }
+            public string CustomerID { get; set; }
+            public int EmployeeID { get; set; }
+            public double Freight { get; set; }
+            public DateTime OrderDate { get; set; }
+            public string ShipCity { get; set; }
+        }
+    }
+}
